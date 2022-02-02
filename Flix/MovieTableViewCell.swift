b73a40480f9175c987a55c08949b91811b4eeeec
johnxminimo <78349183+johnxminimo@users.noreply.github.com>
2022-02-01 18:15:39 -0800

    all files for proj

---
+++ Flix/MovieTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieTableViewCell.swift
+//  Flix
+//
+//  Created by John Minimo on 2/1/22.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
